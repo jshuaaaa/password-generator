7e9e00e7c16cbc48edaa6f9cf5055698dcfdb747
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-06-22 19:53:40 -0400

    linked up checkboxes

--- script.js
+++ script.js
@@ -11,32 +11,46 @@ function writePassword() {
 }
 
 var criteria = document.getElementById("password-criteria");
-criteria.innerHTML += `<input type="checkbox" id="number" name="number" value="no">
+criteria.innerHTML += `<input type="checkbox" id="number" name="number">
 <label for="number"> Would you like numbers in your password?</label><br>
 <input type="checkbox" id="uppercase" name="uppercase">
-<label for="vehicle1">Would you like uppercase letters in your password?</label><br>
+<label for="uppercase">Would you like uppercase letters in your password?</label><br>
 <input type="checkbox" id="special-charachter" name="special-charachter">
-<label for="vehicle1"> Would you like special charachters in your password?</label><br>`;
+<label for="special-charachter"> Would you like special charachters in your password?</label><br>`;
 
 
 generateBtn.addEventListener("click", (writePassword) => {
-  let number = document.querySelectorAll('input[name="number"]:checked');
-  let uppercase = document.querySelectorAll('input[name="uppercase"]:checked');
-  let specialCharachter = document.querySelectorAll('input[name="special-charachter"]:checked');
-  let values = [];
+
+  function makePassword(length) {
+  let number = document.getElementById("number");
+  let uppercase = document.getElementById("uppercase");
+  let specialCharachter = document.getElementById("special-charachter");
+  var result = ""
+  var charachter = "abcdefghijklmnopqrstuvwxyz"
   
-  if(number.checked = true) {
-    console.log('tes')
-  }
+  
+  if(number.checked) {
+    charachter = charachter + '0123456789'
+  } 
+   if(uppercase.checked) {
+    charachter = charachter + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+   } 
+   if(specialCharachter.checked) {
+    charachter = charachter + '!#$&*'
+ } 
 
-  if(uppercase.checked = true) {
-    console.log('tes')
-  }
+ var charactersLength = charachter.length;
 
-  if(specialCharachter.checked = true) {
-    console.log('tes')
-  }
 
+  for(var i = 0; i < length; i++) {
+  result += charachter.charAt(Math.floor(Math.random() * charactersLength));
+  }
+ 
+ 
+  return result;
+}
+
+ console.log(makePassword(20));
 
  });
 // Add event listener to generate button
