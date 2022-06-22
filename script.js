9e520c6a77b4215131aa233e0a280aa303248148
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-06-22 19:19:20 -0400

    finished infrastructure

--- script.js
+++ script.js
@@ -10,5 +10,37 @@ function writePassword() {
 
 }
 
+var criteria = document.getElementById("password-criteria");
+criteria.innerHTML += `<input type="checkbox" id="number" name="number" value="no">
+<label for="number"> Would you like numbers in your password?</label><br>
+<input type="checkbox" id="uppercase" name="uppercase">
+<label for="vehicle1">Would you like uppercase letters in your password?</label><br>
+<input type="checkbox" id="special-charachter" name="special-charachter">
+<label for="vehicle1"> Would you like special charachters in your password?</label><br>`;
+
+
+generateBtn.addEventListener("click", (writePassword) => {
+  let number = document.querySelectorAll('input[name="number"]:checked');
+  let uppercase = document.querySelectorAll('input[name="uppercase"]:checked');
+  let specialCharachter = document.querySelectorAll('input[name="special-charachter"]:checked');
+  let values = [];
+  
+  if(number.checked = true) {
+    console.log('tes')
+  }
+
+  if(uppercase.checked = true) {
+    console.log('tes')
+  }
+
+  if(specialCharachter.checked = true) {
+    console.log('tes')
+  }
+
+
+ });
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+
