3ce64b957fbfbf05af8e73c146d4d1305146c6fa
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-06-24 09:50:35 -0400

    swapped checkboxes to prompts to meet criteria

--- script.js
+++ script.js
@@ -10,38 +10,32 @@ function writePassword() {
 
 }
 
-var criteria = document.getElementById("password-criteria");
-criteria.innerHTML += `<input type="checkbox" id="number" name="number">
-<label for="number"> Would you like numbers in your password?</label><br>
-<input type="checkbox" id="uppercase" name="uppercase">
-<label for="uppercase">Would you like uppercase letters in your password?</label><br>
-<input type="checkbox" id="special-charachter" name="special-charachter">
-<label for="special-charachter"> Would you like special charachters in your password?</label><br>`;
+
 
 
 generateBtn.addEventListener("click", (writePassword) => {
 
   function makePassword(length) {
-  let number = document.getElementById("number");
-  let uppercase = document.getElementById("uppercase");
-  let specialCharachter = document.getElementById("special-charachter");
+  let number = prompt("Do you want numbers in your password y/n?")
+  let uppercase =  prompt("Do you want uppercase letters in your password y/n?")
+  let specialCharachter = prompt("Do you want Special characters in your password y/n?")
   var result = ""
   var charachter = "abcdefghijklmnopqrstuvwxyz"
 
   // Variables are set
   
   
-  if(number.checked) {
+  if(number === 'y' || number === "Y") {
     charachter = charachter + '0123456789'
   } 
-   if(uppercase.checked) {
+   if(uppercase === 'y' || uppercase === "Y") {
     charachter = charachter + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
    } 
-   if(specialCharachter.checked) {
+   if(specialCharachter === 'y' || specialCharachter === "Y") {
     charachter = charachter + '!#$&*'
  } 
 
- // If user checks the checkboxes adds the following characters to the strings
+ // takes user data from the prompts to add value to charachters variable
  
  
  charachtersLength = charachter.length;
